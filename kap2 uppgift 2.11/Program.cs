ac68dd03de73013aed8f55c09650005c2f48a035
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 11:02:01 +0200

    Add project files.

---
+++ kap2 uppgift 2.11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift_2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hur högt kan alma hoppa svara i meter ");
+                string almaText= Console.ReadLine();
+                double alma= double.Parse(almaText);
+                double skillnad = 1 + alma;
+                Console.WriteLine("  elin hoppade  " + skillnad + "  meter högre");
+            }
+    }
+}
+}
